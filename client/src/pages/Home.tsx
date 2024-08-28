424334ae8f8208e4114b138103883bea36750a6e
Renzxs <florencebatol85@gmail.com>
2024-08-28 16:43:46 +0800

    Added animations

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,8 +5,7 @@ import { Apartment, ArrowForwardIos, AttachMoney, LocationOn } from '@mui/icons-
 import PropertiesSlider from '../components/property/PropertiesSlider';
 import dummydata from '../api/dummydata';
 import PropertiesGrid from '../components/property/PropertiesGrid';
-import { animate, motion } from 'framer-motion';
-import fadeInAnimationsVariants from '../animations/fadeInAnimationVariants';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
@@ -25,33 +24,44 @@ const Section1 = () => {
       <section className="w-[90%] flex justify-center items-center flex-wrap gap-10 p-[20px] mt-[80px] max-sm:mt-[100px] max-lg:flex-col">
         <div className='flex-1 flex flex-col justify-between gap-[40px]'>
           {/* Home Title */}
-          <div className='max-lg:flex max-lg:flex-col max-lg:items-center max-lg:text-center'>
+          <motion.div 
+          variants={{ initial: {opacity: 0, x: -100}, 
+          animate: { opacity: 1, x:0 ,transition: { delay: 0.08, duration: 1.2 }}}} 
+          initial="initial" whileInView="animate" className='max-lg:flex max-lg:flex-col max-lg:items-center max-lg:text-center'>
             <h1 className="font-Poppins text-[40px] text-Dark">Real estate for living and investments</h1>
             <Link to={'/'} className='bg-Dark flex justify-center items-center py-[12px] px-[30px] rounded-[30px] hover:opacity-80 w-fit mt-[20px]'>
               <p className='font-Poppins text-[13px] text-Light'>Get Started</p>
             </Link>
-          </div>
+          </motion.div>
 
           {/* Huge Drop downs */}
           <div className='flex flex-col gap-[15px]'>
-            <motion.div variants={fadeInAnimationsVariants} initial="initial" whileInView="animate">
+            <motion.div 
+              variants={{ initial: {opacity: 0, y: 100,},
+              animate: {opacity: 1, y: 1, transition: { delay: 0.05, duration: 1.2 }}}} initial="initial" whileInView="animate">
               <HugeDropDown icon={<LocationOn className='text-Dark'/>} title='Location' value='Metro Manila, Philippines'/>
             </motion.div>
 
-            <motion.div variants={fadeInAnimationsVariants} initial="initial" whileInView="animate">
+            <motion.div 
+              variants={{ initial: {opacity: 0, y: 100,},
+              animate: {opacity: 1, y: 1, transition: { delay: 0.05, duration: 1.2 }}}} initial="initial" whileInView="animate">
               <HugeDropDown icon={<Apartment className='text-Dark'/>} title='Property Type' value='Condominium'/>
             </motion.div>
 
-            <motion.div variants={fadeInAnimationsVariants} initial="initial" whileInView="animate">
+            <motion.div
+                variants={{ initial: {opacity: 0, y: 100,},
+                animate: {opacity: 1, y: 1, transition: { delay: 0.05, duration: 1.2 }}}} initial="initial" whileInView="animate">
               <HugeDropDown icon={<AttachMoney className='text-Dark'/>} title='Price range' value='₱900 - ₱1,500'/>
             </motion.div>
           </div>
         </div>
 
         {/* Home Image */}
-        <div className='flex-[1.8]'>
+        <motion.div variants={{ initial: {opacity: 0, x: 100}, 
+          animate: { opacity: 1, x: 0, transition: { delay: 0.08, duration: 1.2 }}}} 
+          initial="initial" whileInView="animate"className='flex-[1.8]'>
           <img src={HomePageSectionOneImg} alt={HomePageSectionOneImg} className='object-cover rounded-[20px]'/>
-        </div>
+        </motion.div>
       </section>
   );
 }
@@ -95,7 +105,9 @@ const Section2 = () => {
 // Section3
 const Section3 = () => {
   return (
-    <section className="w-[90%] flex justify-center items-center flex-wrap mt-[80px] p-[10px] gap-[50px]">
+    <motion.section variants={{ initial: {opacity: 0}, 
+      animate: { opacity: 1, transition: { delay: 0.08, duration: 1.2 }}}} 
+      initial="initial" whileInView="animate" className="w-[90%] flex justify-center items-center flex-wrap mt-[80px] p-[10px] gap-[50px]">
       {/* Section Title */}
       <div className='flex justify-center items-center w-full'>
         <h1 className="font-Poppins text-[40px] text-Dark text-center max-sm:text-[30px]">Explore inspiring modern houses designs</h1>
@@ -108,15 +120,17 @@ const Section3 = () => {
               <p className='font-Poppins text-[13px] text-Light'>Browse more post</p>
         </Link>
       </div>
-    </section>
+    </motion.section>
   )
 } 
 
 // Section 4
 const Section4 = () => {
   return (
-    <section className="w-[100%] flex justify-center items-center flex-wrap mt-[80px] gap-[50px] bg-Gray py-[50px]">
-      <motion.div variants={fadeInAnimationsVariants} initial="initial" whileInView="animate" className='w-[50%] flex flex-col items-center gap-[20px] max-sm:w-[90%]'>
+    <section className="w-[100%] flex justify-center items-center flex-wrap mt-[80px] gap-[50px] bg-Gray py-[50px] overflow-hidden">
+      <motion.div 
+        variants={{ initial: {opacity: 0, y: 100,}, animate: {opacity: 1, y: 1, transition: { delay: 0.05, duration: 1.2 }}}}
+        initial="initial" whileInView="animate" className='w-[50%] flex flex-col items-center gap-[20px] max-sm:w-[90%]'>
         <h1 className='font-Poppins font-semibold text-center text-Dark text-[40px] max-sm:text-[30px]'>Embark on Your Extraordinary Real Estate Journey Today</h1>
         <p className='font-Poppins text-[13px] text-Dark text-center w-[60%]'>Join the ranks of satisfied homeowners who have entrusted us with their real estate journey</p>
         <Link to={'/'} className='bg-Dark flex justify-center items-center py-[12px] px-[30px] rounded-[30px] hover:opacity-80 w-fit mt-[20px]'>
